2042b2d7ee35ee994921debdbf25b58864a8d5da
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2024-04-20 15:26:23 +0000

    Conversion Operation

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,24 @@
+// Number Conversion
+// let num;
+// num = 100;
+// num = "1000";
+// num = "100pa";
+// num = null;
+// num = undefined;
+
+// let convNum = Number(num)
+// console.log(convNum);
+// console.log( typeof convNum);
+
+// Boolean Conversion
+let isCheck;
+// isCheck = true;
+// isCheck = false;
+// isCheck = 1;
+// isCheck = 0;
+// isCheck = "";
+// isCheck = "true";
+isCheck = "false";
+let isConcCheck = Boolean(isCheck);
+console.log(isConcCheck);
+console.log(typeof isConcCheck);
