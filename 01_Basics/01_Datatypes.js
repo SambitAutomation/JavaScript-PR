f14eabbfce33034b10236d6074696c206943e7b1
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2024-04-20 14:28:56 +0000

    DataTypes

---
+++ 01_Basics/01_Datatypes.js
@@ -0,0 +1,13 @@
+// Primitive DataTypes
+// number
+// bigint
+// string
+// null
+// undefined
+// Symbol
+
+
+// Primitive DataTypes
+// object
+console.log(typeof null)
+console.log(typeof undefined)
