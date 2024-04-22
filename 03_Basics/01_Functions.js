e7e386ff0705c15a9059a515033c0dabf34c50fe
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2024-04-22 16:44:19 +0000

    Functions with objects and array

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -14,3 +14,30 @@ function checkLoggedInUser(userName){
 }
 
 console.log(checkLoggedInUser(""));
+
+// Functions with objects and array
+
+//rest operator ( will show in ...name format and it will converts the values in an array)
+function numbersSetFunc(val1, val2, ...num){
+    return num;
+}
+
+console.log(numbersSetFunc(100, 200, 399,822)); // [ 399, 822 ]
+
+const user = {
+    name: "samt",
+    score: 10
+}
+
+function objectFunction(userObj){
+    console.log(`Username is ${userObj.name} and score is ${userObj.score}`);
+}
+
+objectFunction(user); // Username is samt and score is 10
+
+
+function arrayFunction(arr){
+    return arr[1];
+}
+
+console.log(arrayFunction([100,200,112])); // 200
